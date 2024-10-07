6551f3124180f06a033236c3bf651eb848c96701
ayoubbravo98 <ayoubbravo98@gmail.com>
2024-10-07 16:15:01 +0000

    debug mode

--- app.py
+++ app.py
@@ -63,5 +63,5 @@ def create_video():
     except Exception as e:
         return jsonify({"error": str(e)}), 500
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080, debug=True)
+if __name__ == "__main__":
+    app.run(debug=True, host='0.0.0.0', port=8080)
